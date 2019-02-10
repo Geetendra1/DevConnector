1d25945398f202c5137a6a1a22bb07065e890728
Geetendra1 <43998658+Geetendra1@users.noreply.github.com>
2019-02-10 13:39:59 +0530

    done

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use("/api/posts", posts);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 5000;
